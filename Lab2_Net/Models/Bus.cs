76be87d2ae082ae548e1f5834af17c450232b4e6
Dead4light <129154283+Dead4light@users.noreply.github.com>
2025-04-24 18:23:29 +0300

    Add files via upload

---
+++ Lab2_Net/Models/Bus.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Lab2App.Models
+{
+    public class Bus
+    {
+        public Guid Id { get; set; }
+        public string Model { get; set; }
+        public int Capacity { get; set; }
+    }
+}
---
+++ Lab2_Net/Utils/SyncDemo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+
+namespace Lab2App.Utils
+{
+    public class SyncDemo
+    {
+        private static readonly object _lock = new object();
+        private static SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
+        private static AutoResetEvent _autoResetEvent = new AutoResetEvent(false);
+
+        public static void DemoLock()
+        {
+            lock (_lock)
+            {
+                Console.WriteLine("Critical section (lock) executed");
+            }
+        }
+
+        public static async Task DemoSemaphore()
+        {
+            await _semaphore.WaitAsync();
+            try
+            {
+                Console.WriteLine("Critical section (SemaphoreSlim) executed");
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
+        }
+
+        public static void DemoAutoResetEvent()
+        {
+            _autoResetEvent.WaitOne();
+            Console.WriteLine("Critical section (AutoResetEvent) executed");
+            _autoResetEvent.Set();
+        }
+    }
+}
+
