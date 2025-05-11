f97c822ca568152448f9ce166a1c23c7b8a5e3cd
Dead4light <nonkaivan84@gmail.com>
2025-05-11 19:01:53 +0300

    Виправлена лабораторна робота

---
+++ Zoo/Zoo.Common/Lion.cs
@@ -0,0 +1,18 @@
+﻿namespace Zoo.Common
+{
+    public class Lion : Animal
+    {
+        public double ManeLength { get; set; }
+
+        // Конструктор
+        public Lion(string name, int age, double maneLength) : base(name, age)
+        {
+            ManeLength = maneLength;
+        }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine("Lion roars!");
+        }
+    }
+}
---
+++ Zoo/Zoo.Console/Program.cs
@@ -0,0 +1,43 @@
+﻿using Zoo.Common;
+
+Animal.OnAnimalFed += name => Console.WriteLine($"[EVENT] {name} was just fed!");
+
+var lion = new Lion("Simba", 5, 25.3);
+var parrot = new Parrot("Polly", 2, "Green");
+var zookeeper = new Zookeeper("John Smith", 8);
+
+// CRUD-сервіс
+var animalService = new CrudService<Animal>();
+
+animalService.Create(lion);
+animalService.Create(parrot);
+
+// Вивід усіх
+Console.WriteLine("All animals:");
+foreach (var animal in animalService.ReadAll())
+{
+    Console.WriteLine($"{animal.Name}, Age: {animal.Age}, IsOld: {animal.IsOld()}");
+}
+
+// Виклик методів
+lion.MakeSound();
+parrot.MakeSound();
+
+// Feed з подією
+zookeeper.Feed(parrot);
+
+// Update приклад
+lion.Age = 11;
+animalService.Update(lion);
+
+// Read по Id
+var found = animalService.Read(lion.Id);
+Console.WriteLine($"Found: {found?.Name}, Age: {found?.Age}");
+
+// Remove
+animalService.Remove(parrot);
+Console.WriteLine("After removal:");
+foreach (var animal in animalService.ReadAll())
+{
+    Console.WriteLine(animal.Name);
+}
